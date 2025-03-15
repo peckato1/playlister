peckato1/playlister

commit 2ac25eb51f07095d9d5d29b8970fad20d947fc15

daemon: do not bail after unsuccessfull attempt

playlister/daemon/daemon.py
@@ -62,10 +62,10 @@ class PlaylisterDaemon:
             tracks = loader.fetch(sync_time)
             logger.debug(f'Fetched {len(tracks)} tracks from {loader.station.name}')
             DatabaseWriter(self.db, loader.station).write(sync_time, tracks)
-            self._schedule_next(loader, sync_time)
         except Loader.LoaderException as e:
             logger.exception(f'Failed to fetch data from {loader.station.name}: {e}')
             self._schedule_next(loader, sync_time)
         except Exception as e:
             logger.exception(f'Unexpected error while fetching data from {loader.station.name}: {e}')
-            logger.error(f'Discontinued scheduling loader {loader.station.name}')
+
+        self._schedule_next(loader, sync_time)

commit 07623a6fe4eca9969bd80795d07e9e8b81bd92f5

daemon: correctly register next event action
I forgot to remove one line of code that was causing the next event
action to be registered twice.

Fixes: 2ac25eb51f07095d9d5d29b8970fad20d947fc15

playlister/daemon/daemon.py
@@ -64,7 +64,6 @@ class PlaylisterDaemon:
             DatabaseWriter(self.db, loader.station).write(sync_time, tracks)
         except Loader.LoaderException as e:
             logger.exception(f'Failed to fetch data from {loader.station.name}: {e}')
-            self._schedule_next(loader, sync_time)
         except Exception as e:
             logger.exception(f'Unexpected error while fetching data from {loader.station.name}: {e}')
 