peckato1/playlister

commit f9e6e452c85257f27fb48fedb38fa7a510e88e69

frontend: server side filtering

frontend/src/components/datagrid/DataGrid.tsx
@@ -11,6 +11,7 @@ import {
   type MRT_RowData,
   type MRT_Updater,
   MRT_PaginationState,
+  type MRT_ColumnFiltersState,
 } from 'material-react-table'
 
 interface Props<T extends MRT_RowData> extends DataGridProps<T> {
@@ -32,6 +33,14 @@ export default function DataGrid<T extends MRT_RowData>(props: Props<T>) {
     if (props.setPagination) {
       props.setPagination((prevPagination: MRT_PaginationState) =>
         typeof updater === 'function' ? updater(prevPagination) : updater
+      );
+    }
+  };
+
+  const handleColumnFiltersChange = (updater: MRT_Updater<MRT_ColumnFiltersState>) => {
+    if (props.setColumnFilters) {
+      props.setColumnFilters((prevFilters: MRT_ColumnFiltersState) =>
+        typeof updater === 'function' ? updater(prevFilters) : updater
       );
     }
   };
@@ -59,6 +68,7 @@ export default function DataGrid<T extends MRT_RowData>(props: Props<T>) {
       showFirstButton: true,
       showLastButton: true,
     },
+    onColumnFiltersChange: handleColumnFiltersChange,
     muiFilterDateTimePickerProps: {
       ampm: false,
       ampmInClock: false,

frontend/src/components/datagrid/types.ts
@@ -1,5 +1,6 @@
 import {
   type MRT_PaginationState,
+  type MRT_ColumnFiltersState,
 } from 'material-react-table'
 
 export interface Paginated<T> {
@@ -15,6 +16,7 @@ export interface Paginated<T> {
 export interface DataGridProps<T> {
   paginatedData: Paginated<T>;
   setPagination?: React.Dispatch<React.SetStateAction<MRT_PaginationState>>;
+  setColumnFilters?: React.Dispatch<React.SetStateAction<MRT_ColumnFiltersState>>;
 }
 
 export interface ConcreteDataGridProps<T> extends DataGridProps<T> {

frontend/src/hooks/useApiQuery.ts
@@ -113,8 +113,6 @@ export function useApiQuery(props: ApiQueryProps) {
     })
   }
 
-  console.log(columnFilters)
-
   return {
     query: query,
     setPagination: isPaginationEnabled ? setPagination : undefined,

frontend/src/pages/Home.tsx
@@ -18,6 +18,7 @@ export default function Page() {
         type="trackplayed"
         paginatedData={apiQuery.query.data}
         setPagination={apiQuery.setPagination}
+        setColumnFilters={apiQuery.setColumnFilters}
       />
     </>
   );

frontend/src/pages/InterpretDashboard.tsx
@@ -18,6 +18,7 @@ export default function Page() {
         type="interpret"
         paginatedData={apiQuery.query.data}
         setPagination={apiQuery.setPagination}
+        setColumnFilters={apiQuery.setColumnFilters}
       />
     </>
   );

frontend/src/pages/InterpretPlayed.tsx
@@ -14,6 +14,7 @@ export default function InterpretPlayed() {
       type="trackplayed"
       paginatedData={apiQuery.query.data}
       setPagination={apiQuery.setPagination}
+      setColumnFilters={apiQuery.setColumnFilters}
       enableColumnFilters={{
         'interpret': false,
       }}

frontend/src/pages/InterpretTrackList.tsx
@@ -14,6 +14,7 @@ export default function InterpretTrackList() {
       type="track"
       paginatedData={apiQuery.query.data}
       setPagination={apiQuery.setPagination}
+      setColumnFilters={apiQuery.setColumnFilters}
       enableColumnFilters={{'interpret': false}}
     />
   )

frontend/src/pages/Station.tsx
@@ -22,6 +22,7 @@ export default function Page() {
         type="trackplayed"
         paginatedData={apiQueryPlayed.query.data}
         setPagination={apiQueryPlayed.setPagination}
+        setColumnFilters={apiQueryPlayed.setColumnFilters}
         enableColumnFilters={{
           'station': false,
           'synced': false,

commit 6a66863b41f42f8468df95078d8fe12501542e54

frontend: forgot to set manual filtering
Fixes: f9e6e45 ("frontend: server side filtering")

frontend/src/components/datagrid/DataGrid.tsx
@@ -68,6 +68,7 @@ export default function DataGrid<T extends MRT_RowData>(props: Props<T>) {
       showFirstButton: true,
       showLastButton: true,
     },
+    manualFiltering: true,
     onColumnFiltersChange: handleColumnFiltersChange,
     muiFilterDateTimePickerProps: {
       ampm: false,